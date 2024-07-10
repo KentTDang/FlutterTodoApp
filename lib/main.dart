cc16c403335c046287257a49c0d2f27f748d2560
KentTDang <kent_2015@outlook.com>
2024-07-10 00:05:44 -0400

    ✅

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
 import 'package:firebase_core/firebase_core.dart';
-import 'package:todo_app/pages/home_page.dart';
+// import 'package:todo_app/pages/home/home_page.dart';
 import 'package:todo_app/widget_tree.dart';
 // import 'package:todo_app/widget_tree.dart';
 import 'firebase_options.dart';
 import 'package:flutter/material.dart';
-import 'package:todo_app/pages/welcome_page.dart';
+// import 'package:todo_app/pages/home/welcome_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
---
+++ lib/pages/auth/register_page.dart
@@ -0,0 +1,178 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import '../../auth.dart';
+
+class RegisterPage extends StatefulWidget {
+  final VoidCallback onToggle;
+
+  const RegisterPage({super.key, required this.onToggle});
+
+  @override
+  State<RegisterPage> createState() => _RegisterPageState();
+}
+
+class _RegisterPageState extends State<RegisterPage> {
+  final TextEditingController _controllerEmail = TextEditingController();
+  final TextEditingController _controllerPassword = TextEditingController();
+  String? errorMessage = '';
+
+  Future<void> createUserWithEmailAndPassword() async {
+    try {
+      await Auth().createUserWithEmailAndPassword(
+        email: _controllerEmail.text,
+        password: _controllerPassword.text,
+      );
+      _controllerEmail.clear();
+      _controllerPassword.clear();
+    } on FirebaseAuthException catch (e) {
+      setState(() {
+        errorMessage = e.message;
+      });
+    }
+  }
+
+  Widget _errorMessage() {
+    return Text(errorMessage == '' ? '' : 'Hmm ? $errorMessage');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            height: double.infinity,
+            width: double.infinity,
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(colors: [
+                Color(0xffB81736),
+                Color(0xff281537),
+              ]),
+            ),
+            child: const Padding(
+              padding: EdgeInsets.only(top: 60.0, left: 22),
+              child: Text(
+                'Create\nYour Account!',
+                style: TextStyle(
+                  fontSize: 30,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 200.0),
+            child: Container(
+              decoration: const BoxDecoration(
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(40), topRight: Radius.circular(40)),
+                color: Colors.white,
+              ),
+              height: double.infinity,
+              width: double.infinity,
+              child: Padding(
+                padding: const EdgeInsets.only(left: 18.0, right: 18),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const TextField(
+                      decoration: InputDecoration(
+                        label: Text(
+                          'Display Name',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Color(0xffB81736),
+                          ),
+                        ),
+                      ),
+                    ),
+                    TextField(
+                      controller: _controllerEmail,
+                      decoration: const InputDecoration(
+                        label: Text(
+                          'Email',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Color(0xffB81736),
+                          ),
+                        ),
+                      ),
+                    ),
+                    TextField(
+                      controller: _controllerPassword,
+                      decoration: const InputDecoration(
+                        label: Text(
+                          'Password',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Color(0xffB81736),
+                          ),
+                        ),
+                      ),
+                      obscureText: true,
+                    ),
+                    const SizedBox(height: 10),
+                    if (errorMessage != null && errorMessage!.isNotEmpty)
+                      Text(errorMessage!, style: const TextStyle(color: Colors.red)),
+                    const SizedBox(height: 70),
+                    GestureDetector(
+                      onTap: createUserWithEmailAndPassword,
+                      child: Container(
+                        height: 55,
+                        width: 300,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(30),
+                          gradient: const LinearGradient(colors: [
+                            Color(0xffB81736),
+                            Color(0xff281537),
+                          ]),
+                        ),
+                        child: const Center(
+                          child: Text(
+                            'REGISTER',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 80),
+                    Align(
+                      alignment: Alignment.bottomRight,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        children: [
+                          const Text(
+                            "Have an account?",
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold, color: Colors.grey),
+                          ),
+                          GestureDetector(
+                            onTap: widget.onToggle,
+                            child: const Text(
+                              "Sign In",
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 17,
+                                color: Colors.black,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
